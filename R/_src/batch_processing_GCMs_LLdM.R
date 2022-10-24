49ba75fcf5f11ddfeb734aa6bafccd6697b4957a
davemfish <davemfish@gmail.com>
2022-10-24 10:01:39 -0400

    cleaning up R batch script for readability.

--- R/_src/batch_processing_GCMs_LLdM.R
+++ R/_src/batch_processing_GCMs_LLdM.R
@@ -1,7 +1,11 @@
 ######### Modifiable data for particular application #####################################################
 
-mFolder = "G:/Shared drives/GCM_Climate_Tool"
-setwd(mFolder)
+mFolder = "H:/Shared drives/GCM_Climate_Tool"
+rFolder <- "C:/Users/dmf/projects/gcm-project/test-results"
+shp_file_background <- paste0("/required_files/OBSERVATIONS/LLdM_AOI2/SHP/Basin_LldM.shp") # Basin Shapefile
+BootstrapRuns <- 1 # number of iterations to perform of the bootstrapping routine
+CLIMATE_VARS = c("pr", "tas")
+# setwd(mFolder)
 
 ### ------------------------------
 ### SITE SPECIFIC DATA SOURCES: climate observations
@@ -12,40 +16,46 @@ observation_files_lock = c(daily = "/required_files/OBSERVATIONS/LLdM_AOI2/serie
 
 
 # in the "BoundingBoxClimaZones.csv". add rows to include different regions
-bboxes <- read.csv(paste0(mFolder,"/required_files/OBSERVATIONS/LLdM_AOI2/BoundingBoxClimaZones.csv"),stringsAsFactors=F)
-rFolder <- "/Results/LLdM_test/"
-rFolderMain = paste0("/Results/LLdM_test/")  # Results Folder
+bboxes <- read.csv(
+  file.path(mFolder, "/required_files/OBSERVATIONS/LLdM_AOI2/BoundingBoxClimaZones.csv"),
+  stringsAsFactors=F)
+
+# rFolderMain = paste0("/Results/LLdM_test/")  # Results Folder
 
 gcmFolders = "GCMs/" #df3$File[1]
 enso_signal_file = "/required_files/Other/ONI_SIGNAL.csv" # ENSO EL NINO-LA NINA CYCLE DATA
 svd_analysis_file = "/required_files/Other/SVD_Table_Comp1_Pac_Atl.csv" #Pacific and Atlantic SST Oscillation Data
 
+df_dumm = read.csv(
+  file.path(mFolder, 'required_files/Other/Archive_list_batch_LLdM_Nov2021 - CMIP6.csv'),
+  header=T)
+df = subset(df_dumm,df_dumm$DoIt==1)
+df$BaseDay <- NA
+df$BaseMonth <- NA
+df$BaseYear <- NA
+df$LeapFix <- NA
+
+models = unique(df$Model)
+ensembles = unique(df$Ensemble)
+experiment = unique(df$Experiment)
+fut_all = unique(df$Experiment)
+fut_all = fut_all[fut_all != "Historical"]
+var_all = c("pr")
+
+if (!dir.exists(file.path(rFolder))) {
+  dir.create(file.path(rFolder))
+}
+save(df, file = "df.Rda")
 
 for(r in 1:1) #1:nrow(bboxes) 
 {
   region <- bboxes$Descripcion[r] # name of your basin or region for use in output graphics
-  shp_file_background <- paste0("/required_files/OBSERVATIONS/LLdM_AOI2/SHP/Basin_LldM.shp")# Basin Shapefile
-  bBox = c(latMin = bboxes$ymin[r],latMax = bboxes$ymax[r],lonMin = bboxes$xmin[r],lonMax = bboxes$xmax[r])  # Bounding Box of Study Region
-  
+  bBox = c(
+    latMin = bboxes$ymin[r],
+    latMax = bboxes$ymax[r],
+    lonMin = bboxes$xmin[r],
+    lonMax = bboxes$xmax[r])  # Bounding Box of Study Region
   ## enter -999 for the bounding box values to use the shapefile to define the bounding box.
-  BootstrapRuns <- 1 # number of iterations to perform of the bootstrapping routine
-  df_dumm = read.csv(paste0(mFolder,'/required_files/Other/Archive_list_batch_LLdM_Nov2021 - CMIP6.csv'),header=T) #No reason to complicate
-  df = subset(df_dumm,df_dumm$DoIt==1)
-  df$BaseDay <- NA
-  df$BaseMonth <- NA
-  df$BaseYear <- NA
-  df$LeapFix <- NA
-  
-  models = unique(df$Model)
-  ensembles = unique(df$Ensemble)
-  experiment = unique(df$Experiment)
-  fut_all = unique(df$Experiment)
-  fut_all = fut_all[fut_all != "Historical"]
-  var_all = c("pr")
- 
-  if(dir.exists(paste0(mFolder, rFolder))) {save(df, file = paste0(mFolder,rFolder,"df.Rda"))} else
-  {dir.create(paste0(mFolder,rFolder))
-    save(df, file = paste0(mFolder,rFolder,"df.Rda"))}
   
   #-----------------------------------------------------------
   # PART ONE: Loops through the catalog and reads the netcdfs and the local data (pr and tas):
@@ -53,25 +63,27 @@ for(r in 1:1) #1:nrow(bboxes)
   start1 <- Sys.time()
   
   for (model in models) {
-    rFolder = paste0(paste0(rFolderMain, model, ""))
-    if(dir.exists(paste0(mFolder, rFolder))) {} else
-    {dir.create(paste0(mFolder,rFolder))}
-    save(df, file = paste0(mFolder,rFolder,"df.Rda"))
+    model_results_path = file.path(rFolder, model)
+    # rFolder = paste0(paste0(rFolderMain, model, ""))
+    if (!dir.exists(model_results_path)) {
+      dir.create(model_results_path)
+    }
+    save(df, file = file.path(model_results_path, "df.Rda")) # copied main df to model folder?
     
     df1 = subset(df, Model == model)
     exp_model = unique(df1$Experiment)
     
-    for (ensemble in ensembles){
+    for (ensemble in ensembles) {
       df2 = subset(df1, ensemble == Ensemble)
       
-      for (varName in c("pr","tas")) {                           #add more available vars in the list as: varName in c("pr","tas", ...))
+      for (varName in CLIMATE_VARS) {
         df3 = varName #subset(df2, Variable == varName)
         mv = 1 #df3$Moving.Year[1]  #moving year to calculate transition matrix
         varLabel = varName ## (lazy, fix if wanted). .. "Precipitation" #df3$Alias[1]
         observation_files = observation_files_lock
-        observation_files[1] = gsub("VARIABLE",varName,observation_files_lock[1])
-        observation_files[2] = gsub("VARIABLE",varName,observation_files_lock[2])
-        load(paste0(mFolder,rFolder,"df.Rda"),verbose = TRUE)
+        observation_files[1] = gsub("VARIABLE", varName, observation_files_lock[1])
+        observation_files[2] = gsub("VARIABLE", varName, observation_files_lock[2])
+        load(file.path(model_results_path, "df.Rda"), verbose = TRUE)
         
         ## Temp
         # experiment = experiment[1] #"historical"
@@ -95,13 +107,13 @@ for(r in 1:1) #1:nrow(bboxes)
         {
           try(dev.off())
         }
-        load(paste0(mFolder,rFolder,"df.Rda"))
+        load(file.path(mFolder, rFolder, "df.Rda"))
         print(varName)
       } # next variable
     } # next ensemble
  
     
-    load(paste0(mFolder,rFolder,"df.Rda"))
+    load(file.path(mFolder, rFolder, "df.Rda"))
     df1 = subset(df, Model == model)
     exp_model = unique(df1$Experiment)
     exp_model <- exp_model[exp_model != "historical"] # excludes historical experiment from following functions to just have futures
@@ -141,13 +153,13 @@ for(r in 1:1) #1:nrow(bboxes)
   #-----------------------------------------------------------
   
   for (model in models) {
-  
-    load(paste0(mFolder,rFolder,"df.Rda"))
+    model_results_path = file.path(rFolder, model)
+    load(file.path(model_results_path, "df.Rda"))
     df1 = subset(df, Model == model)
     exp_model = unique(df1$Experiment)
     exp_model <- exp_model[exp_model != "historical"] # excludes historical experiment from following functions to just have futures
 
-    rFolder = paste0(paste0(rFolderMain, model, ""))
+    # rFolder = paste0(paste0(rFolderMain, model, ""))
     
     # Optional: update of extremes Obs based (data_d) of GCM Results.
     # The future reference year represents the upper boundary year for which new extremes are derived
@@ -168,7 +180,7 @@ for(r in 1:1) #1:nrow(bboxes)
     for (n in 1:BootstrapRuns){
       try(knn_bootstrap(refDate = c(month = df$BaseMonth[rcp_pr_ind], day = df$BaseDay[rcp_pr_ind], year = df$BaseYear[rcp_pr_ind]),
                         main_folder = mFolder,
-                        results_folder = rFolder,
+                        results_folder = model_results_path,
                         modelName = model,              #"CCSM4",
                         futures = experiment2,
                         varName = "pr",
