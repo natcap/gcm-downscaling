ea9b6e65b627b3ef39c495245583fd3f14be643f
davemfish <davemfish@gmail.com>
2022-11-08 08:19:34 -0500

    moved function from its own module into knn module

--- src/knn.py
+++ src/knn.py
@@ -9,8 +9,6 @@ import pandas
 import pygeoprocessing
 import xarray
 
-from .joint_probability import tri_state_joint_probability
-
 LOGGER = logging.getLogger(__name__)
 
 LOWER_BOUND = 2.0e-06  # units of the GCM pr variable
@@ -30,6 +28,44 @@ REF_PERIOD_END_DATE = '2014-12-31'
 PREDICTION_PERIOD_START_DATE = '2015-01-01'
 PREDICTION_PERIOD_END_DATE = '2016-01-31'
 DATA_STORE_PATH = 'H://Shared drives/GCM_Climate_Tool/required_files'
+
+
+def tri_state_joint_probability(timeseries, lower_bound, upper_bound):
+    """Calculate probabilities of state transitions for consecutive
+       values in timeseries.
+
+    Args:
+        timeseries (numpy.array): a 1-dimensional array.
+        lower_bound (float): the lower boundary of the middle bin.
+        upper_bound (float): the upper boundary (inclusive) of the middle bin.
+
+    Returns:
+        (numpy.array): a 2-dimensional array of joint probabilities.
+    """
+    jp_matrix = numpy.zeros([3, 3])
+
+    a = timeseries[:-1]
+    b = timeseries[1:]
+
+    a_low_mask = a <= lower_bound
+    jp_matrix[0, 0] = numpy.count_nonzero(b[a_low_mask] <= lower_bound)
+    jp_matrix[0, 2] = numpy.count_nonzero(b[a_low_mask] > upper_bound)
+    jp_matrix[0, 1] = numpy.count_nonzero(
+        numpy.count_nonzero(a_low_mask) - jp_matrix[0, 0] - jp_matrix[0, 2])
+
+    a_high_mask = a > upper_bound
+    jp_matrix[2, 0] = numpy.count_nonzero(b[a_high_mask] <= lower_bound)
+    jp_matrix[2, 2] = numpy.count_nonzero(b[a_high_mask] > upper_bound)
+    jp_matrix[2, 1] = numpy.count_nonzero(
+        numpy.count_nonzero(a_high_mask) - jp_matrix[2, 0] - jp_matrix[2, 2])
+
+    a_med_mask = (a > lower_bound) & (a <= upper_bound)
+    jp_matrix[1, 0] = numpy.count_nonzero(b[a_med_mask] <= lower_bound)
+    jp_matrix[1, 2] = numpy.count_nonzero(b[a_med_mask] > upper_bound)
+    jp_matrix[1, 1] = numpy.count_nonzero(
+        numpy.count_nonzero(a_med_mask) - jp_matrix[1, 0] - jp_matrix[1, 2])
+
+    return (jp_matrix / numpy.sum(jp_matrix))
 
 
 def state_transition_table(array, lower_bound, upper_bound):
--- src/tests/test_downscaling.py
+++ src/tests/test_downscaling.py
@@ -125,7 +125,7 @@ class TestJointProbability(unittest.TestCase):
 
     def test_tri_state_joint_probability(self):
         """Test tri_state_joint_probability.py."""
-        from .. import joint_probability
+        from .. import knn
 
         timeseries = numpy.array([0, 0, 0, 5, 2, 1, 0, 0, 9, 8, 0])
         lower_bound = 1.5
@@ -138,7 +138,7 @@ class TestJointProbability(unittest.TestCase):
             [0.1, 0.0, 0.1]
         ])
 
-        actual_matrix = joint_probability.tri_state_joint_probability(
+        actual_matrix = knn.tri_state_joint_probability(
             timeseries, lower_bound, upper_bound)
 
         numpy.testing.assert_array_equal(actual_matrix, expected_matrix)
