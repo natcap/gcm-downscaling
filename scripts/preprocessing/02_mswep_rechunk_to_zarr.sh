f40eab03e6cbb98f641730eaf5983e51853265cd
davemfish <davemfish@gmail.com>
2023-01-19 09:21:38 -0500

    organizing scripts

--- scripts/preprocessing/02_mswep_rechunk_to_zarr.sh
+++ scripts/preprocessing/02_mswep_rechunk_to_zarr.sh
@@ -2,8 +2,8 @@
 #
 #SBATCH --time=8:00:00
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=20
-#SBATCH --mem-per-cpu=8000M
+#SBATCH --cpus-per-task=10
+#SBATCH --mem-per-cpu=20000M
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=davefisher@stanford.edu
 #SBATCH --partition=hns,normal
@@ -18,5 +18,6 @@ WORKSPACE_DIR="$L_SCRATCH/$WORKSPACE_NAME"
 set -x  # Be eXplicit about what's happening.
 FAILED=0
 singularity run \
-    docker://$CONTAINER python scripts/concat_mswep.py \
-    --n_workers=20 \
+    docker://$CONTAINER python scripts/preprocessing/02_mswep_rechunk_to_zarr.py \
+    --n_workers=10 \
+    --max_mem=20
